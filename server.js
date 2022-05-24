59454d2d1f8697173a978e231b742d05b13dc672
jsanchez0112 <edsaja12@gmail.com>
2022-05-24 13:51:37 -0500

    third

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ mongoose.connect(process.env.DATABASE_URL,{
       useNewUrlParser: true,
       useUnifiedTopology: true
 });
+
+//Database connection Error/Success
+//Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected' , () => console.log('mongo disconnected'));
+
+//middleware
+//body parser middleware: give us access to req.body
+app.use(express.urlencoded({extended: true}))
+
+//Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
